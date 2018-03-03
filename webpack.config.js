e7fa2153463b00463133f9a243f46bc6b197f465
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-03-03 13:13:12 -0400

    Config chunks for webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,52 @@
-
 const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
- entry: './src/app.js',
- output: {
-  filename: 'bundle.js',
-  path: path.join(__dirname, 'dist')
- },
- module:{
-  rules:[{
-   loader: 'babel-loader',
-   test: /\.js$/,
-   exclude: /node_modules/
-  }, {
-   test: /\.s?css/,
-   use: [
-    'style-loader',
-    'css-loader',
-    'sass-loader'
-   ]
-  }]
- },
- devtool: 'cheap-module-eval-source-map',
- devServer: {
-  contentBase: path.join(__dirname, 'dist'),
-  historyApiFallback: true
+module.exports = (env) => {
+ const isProduction = env === 'production';
+ const CSSExtract = new ExtractTextPlugin('styles.css');
+
+ return {
+  entry: {
+   app: ['./src/App.js'],
+   vendor: ['react', 'react-dom', 'react-router-dom'],
+  },
+  output: {
+   filename: '[name].bundle.js',
+   path: path.join(__dirname, 'dist')
+  },
+  optimization: {
+   splitChunks: {
+    cacheGroups: {
+     vendor: {
+      name: "vendor",
+      test: "vendor",
+      enforce: true
+     }
+    }
+   }
+  },
+  plugins: [
+   // CSSExtract
+  ],
+  module: {
+   rules: [{
+    loader: 'babel-loader',
+    test: /\.js$/,
+    exclude: /node_modules/
+   }, {
+    test: /\.s?css/,
+    use: [
+     'style-loader',
+     'css-loader',
+     'sass-loader'
+    ]
+   }]
+  },
+  devtool: isProduction ? 'source-map' : 'inline-source-map',
+  devServer: {
+   contentBase: path.join(__dirname, 'dist'),
+   historyApiFallback: true
+  }
  }
 }
