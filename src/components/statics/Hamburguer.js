aa00e95e77ebb4b7f8a19736a2e0586e337c80cb
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-03-05 22:26:51 -0400

    Added mobile CSS to sidebar and hamburguer.

---
+++ src/components/statics/Hamburguer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Hamburguer extends Component {
+  render() {
+    return (
+      <div className="app__hamburguer">
+        <i className="app__hamburguer--icon icon-menu" />
+      </div>
+    );
+  }
+}
+
+export default Hamburguer;
--- src/routes/app.router.js
+++ src/routes/app.router.js
@@ -10,6 +10,7 @@ import {
 // Import Components
 import Nav from "../components/statics/Navigation";
 import Footer from "../components/statics/Footer";
+import Hamburguer from "../components/statics/Hamburguer";
 
 // Pages
 import Homepage from "../components/pages/Homepage";
@@ -20,6 +21,7 @@ import Calculator from "../components/ui/Calculator/Calculator";
 const AppRouter = () => (
   <Router>
     <div className="app">
+      <Hamburguer />
       <div className="app__static">
         <Nav />
         <div className="app__content">
