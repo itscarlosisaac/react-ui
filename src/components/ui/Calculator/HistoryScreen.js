733954cd87ebdf07da0dffbd6cfc804800509816
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-03-12 12:05:22 -0400

    Fixed history error.

--- src/components/ui/Calculator/HistoryScreen.js
+++ src/components/ui/Calculator/HistoryScreen.js
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 
 class HistoryScreen extends Component {
   render() {
-    const op = this.props.operation; 
-    return <div className={ op !== "" ? "calculator__app--results" : "calculator__app--results calculator__app--results--empty" }>{op}</div>; 
+    return <div className="calculator__app--history">{this.props.history}</div>;
   }
 }
 
